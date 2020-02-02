2badeed13c2c29778ca33cd428fbf3379f6e8625
samarrero <marreros@ufl.edu>
2020-02-02 05:31:54 -0500

    updated scraped tweets

--- TweetScraper.py
+++ TweetScraper.py
@@ -46,7 +46,7 @@ def getTweets(screenName):
         outTweets = [[tweet.id_str, tweet.created_at, tweet.full_text] for tweet in tweetList]
     
         #writing to the csv file
-        with open(screenName + '_Tweets.csv', 'w', encoding='utf8') as f:
+        with open('Tweets Scraped/' + screenName + '_Tweets.csv', 'w', encoding='utf8') as f:
             writer = csv.writer(f)
             writer.writerow(['ID', 'Timestamp', 'Content'])
             writer.writerows(outTweets)
