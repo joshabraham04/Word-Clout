dcd05fbd5a011209148e4bd9f26b3fe7b166822f
Josh Abraham <joshabraham@ufl.edu>
2020-02-02 06:14:39 -0500

    Added comments to the main functions

--- WordCloudGenerator.py
+++ WordCloudGenerator.py
@@ -1,20 +1,13 @@
+###################################
+# RUN THE FUNCTION FROM THIS FILE #
+###################################
+
 # importing all the libraries required for this WordCloud generator
 import numpy as np
-import pandas as pd
-from os import path
 from PIL import Image
 import matplotlib.pyplot as plt
 import wordcloud
 from ElectRobo import wordMap, imageName, candidateName
-
-# old
-# DataFrame = pd.read_csv("SampleCSV.csv")
-# DataFrame.head()
-# DataFrame[["name", "fruit", "number"]].head()
-# fruit = DataFrame.groupby("name")
-# fruit.describe().head()
-# fruit.mean().sort_values(by="number", ascending=False).head()
-# text = DataFrame.fruit[2]
 
 mask = np.array(Image.open("Masks/" + imageName))
 
